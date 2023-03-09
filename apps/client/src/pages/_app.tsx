247564041f934944e11242c6c2e43ce58d87e0aa
Idhibhat Pankam <marginpankam@gmail.com>
2023-03-09 22:42:24 +0700

    Revert "feat: add intania colors"
    This reverts commit 80e7057c94e46cc52c5bb135609f1e570973e7e5.

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,16 +1,13 @@
-import "$styles/global.scss";
 import type { AppProps } from "next/app";
 import Head from "next/head";
+import "$styles/global.scss";
 
 function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
-        <title>Intania Department Calculator</title>
-        <meta
-          content="Calculate your required freshmen grades here"
-          name="description"
-        />
+        <title>App Name</title>
+        <meta content="App description" name="description" />
       </Head>
 
       <Component {...pageProps} />
