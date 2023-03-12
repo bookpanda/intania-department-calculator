e38e968da2421e52fc410b513b670fce51917ad3
Idhibhat Pankam <marginpankam@gmail.com>
2023-03-12 23:38:26 +0700

    feat: favicon

--- apps/client/src/pages/_document.tsx
+++ apps/client/src/pages/_document.tsx
@@ -12,6 +12,27 @@ export default function Document() {
           href="https://fonts.googleapis.com/css?family=IBM+Plex+Sans+Thai:r,b&display=swap"
           rel="stylesheet"
         />
+        <link
+          href="/apple-touch-icon.png"
+          rel="apple-touch-icon"
+          sizes="180x180"
+        />
+        <link
+          href="/favicon-32x32.png"
+          rel="icon"
+          sizes="32x32"
+          type="image/png"
+        />
+        <link
+          href="/favicon-16x16.png"
+          rel="icon"
+          sizes="16x16"
+          type="image/png"
+        />
+        <link href="/site.webmanifest" rel="manifest" />
+        <link color="#5bbad5" href="/safari-pinned-tab.svg" rel="mask-icon" />
+        <meta content="#da532c" name="msapplication-TileColor" />
+        <meta content="#ffffff" name="theme-color" />
       </Head>
       <body>
         <Main />
